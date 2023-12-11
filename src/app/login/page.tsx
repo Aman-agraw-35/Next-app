c6c480094229bf345f14ae1852bcd6f0ea5040a1
Aman Agrawal <aman.agraw.35@gmail.com>
2023-12-12 01:37:29 +0530

    making the frontend part

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,38 @@
 "use client";
 import React,{useState,useEffect} from "react";
-
+import {useRouter} from "next/navigation";
+import Link from "next/link";
+import { NextResponse } from "next/server";
 
 export default  function Login(){
-
+const router = useRouter();
     
 const [user,setUser] = useState({
     password: "",
     email: ""
 })
 
+async function   handleClick(e:any){
+    try {
+ e.preventDefault();
+ console.log(user);
+ 
+ router.push("/profile");
+    } catch (error:any) {
+      NextResponse.json(error);
+    }
+}
+
     return (
-        <div className="h-[100vh] w-[100vh] items-center flex relative ">
-            <form className="h-full w-[200px] flex flex-col text-center justify-center align-middle " action="submit">
+        <div className="h-[100vh] w-full items-center flex relative flex-col  ">
+            <form  onSubmit={handleClick}  className="h-max mb-4 w-[200px] flex flex-col text-center justify-center  " action="submit">
                 <label htmlFor="email">Email </label>
-                <input className=" justify-center" type="text" /><br />
+                <input onChange={(e) => setUser({...user , email : e.target.value })} value={user.email} placeholder="Email" className="text-black justify-center" type="text" /><br />
                 <label htmlFor="password">Password </label>
-                <input type="password" />
-                <button type="submit" className="mt-4 border-2 border-white rounded">Submit</button>
+                <input type="password" className="text-black" onChange={(e) => setUser({...user , password : e.target.value })} value={user.password} placeholder="Password"  />
+                <button type="submit" className="mt-4  border-2 border-white rounded">Submit</button>
             </form>
+            <Link href="/signup">Visit Signup Page</Link>
         </div>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,28 +1,63 @@
 "use client";
 import React,{useState,useEffect} from "react";
-
-
+import {useRouter} from "next/navigation";
+import Link from "next/link";
+import { NextResponse } from "next/server";
 
 export default  function Signup(){
 
-
+   const router = useRouter();
     const [user,setUser] = useState({
         username: "",
         password: "",
         email: ""
     })
     
+    async function   handleClick(e:any){
+        try {
+     e.preventDefault();
+     console.log(user);
+     
+     router.push("/login");
+        } catch (error:any) {
+          NextResponse.json(error);
+        }
+    }
+    
     return (
-        <div className="h-[100vh] w-full items-center flex relative ">
-            <form className="h-full w-[200px] flex flex-col text-center justify-center align-middle " action="submit">
+        <div className="h-[100vh] w-full items-center flex relative flex-col ">
+            <form onSubmit={handleClick} className="h-max mb-4 w-[200px] flex flex-col text-center justify-center align-middle " action="submit">
+                
                 <label htmlFor="Username">Username </label>
-                <input className=" justify-center" type="text" /><br />
+                <input 
+                onChange={(e) => setUser({...user , username : e.target.value })} 
+                value={user.username} placeholder="Username" 
+                className=" justify-center text-black" 
+                type="text" />
+                <br/>
+
                 <label htmlFor="email">Email </label>
-                <input className=" justify-center" type="text" /><br />
+                <input 
+                onChange={(e) => setUser({...user , email : e.target.value })} 
+                value={user.email} placeholder="Email" 
+                className=" justify-center text-black" 
+                type="text" 
+                />
+                <br/>
+
                 <label htmlFor="password">Password </label>
-                <input type="password" />
-                <button type="submit" className="mt-4 border-2 border-white rounded">Submit</button>
+                <input 
+                type="password" 
+                onChange={(e) => setUser({...user , password : e.target.value })} 
+                value={user.password} 
+                placeholder="Password"
+                className="text-black" 
+                />
+
+
+                <button  type="submit"  className="mt-4 border-2 border-white rounded">Submit</button>
             </form>
+            <Link href="/login">Visit Login Page</Link>
         </div>
     )
 }
